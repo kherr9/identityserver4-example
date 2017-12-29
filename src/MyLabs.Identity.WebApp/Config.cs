03374ede08de5c8e69b4e7791a9ac110fe196601
Kevin Herr <kherr9@gmail.com>
2017-12-29 14:00:05 -0600

    cfg resources and clients

---
+++ src/MyLabs.Identity.WebApp/Config.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace MyLabs.Identity.WebApp
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
